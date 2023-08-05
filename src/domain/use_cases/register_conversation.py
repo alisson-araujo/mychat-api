137fc6b8d4a713ef580b01de34960e2defff4ef6
Alisson Araujo <alisson.ads@hotmail.com>
2023-08-05 14:53:03 -0300

    fix: returning int instead of dict

--- src/domain/use_cases/register_conversation.py
+++ src/domain/use_cases/register_conversation.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Dict
 
 
 class RegisterConversation(ABC):
     @abstractmethod
-    def register(self, name: str) -> Dict:
+    def register(self, name: str) -> int:
         pass
