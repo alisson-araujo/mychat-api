bb49bd7569e38a978e8532db9f1e239ca2cba589
Alisson Araujo <alisson.ads@hotmail.com>
2023-08-13 13:10:22 -0300

    fix: using async await in requests

--- src/main/adapters/request_adapter.py
+++ src/main/adapters/request_adapter.py
@@ -4,10 +4,16 @@ from src.presentation.http_types.http_request import HttpRequest
 from src.presentation.http_types.http_response import HttpResponse
 
 
-def request_adapter(request: FastAPIRequest, controller: Callable) -> HttpResponse:
+async def request_adapter(
+    request: FastAPIRequest, controller: Callable
+) -> HttpResponse:
+    if request.method == "POST":
+        body = await request.json()
+    else:
+        body = {}
     http_request = HttpRequest(
         headers=request.headers,
-        body=request.body,
+        body=body,
         query_params=request.query_params,
         path_params=request.path_params,
         url=request.url,
--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Request
 
 from ..adapters.request_adapter import request_adapter
 from ..composers.get_messages_composer import get_messages_composer
+from ..composers.send_message_composer import send_message_composer
+from src.presentation.http_types.http_request import HttpRequest
 
 from src.errors.error_handler import handle_errors
 
@@ -9,9 +11,19 @@ router = APIRouter()
 
 
 @router.get("/messages")
-def get_messages(request: Request):
+async def get_messages(request: Request):
     try:
-        http_response = request_adapter(request, get_messages_composer())
+        http_response = await request_adapter(request, get_messages_composer())
+    except Exception as exception:
+        http_response = handle_errors(exception)
+
+    return http_response
+
+
+@router.post("/send-message")
+async def send_message(request: Request):
+    try:
+        http_response = await request_adapter(request, send_message_composer())
     except Exception as exception:
         http_response = handle_errors(exception)
 
