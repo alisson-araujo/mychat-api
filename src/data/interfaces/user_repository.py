2173a7c386167d1018fa4f120b27e831d924a743
Alisson Araujo <alisson.ads@hotmail.com>
2023-09-03 23:19:32 -0300

    feat: method for fetching a list of users

--- src/data/interfaces/user_repository.py
+++ src/data/interfaces/user_repository.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from src.db.entities.user import User
+from typing import List
 
 
 class UserRepositoryInterface(ABC):
@@ -9,4 +10,8 @@ class UserRepositoryInterface(ABC):
 
     @abstractmethod
     def get_user_by_phone(self, phone: str) -> User:
+        pass
+
+    @abstractmethod
+    def get_users(self, list_of_phones: list) -> List:
         pass
--- src/db/repositories/user_repository.py
+++ src/db/repositories/user_repository.py
@@ -1,6 +1,14 @@
 from ..settings.connection import DBConnectionHandler
 from ..entities.user import User
 from src.data.interfaces.user_repository import UserRepositoryInterface
+from typing import List
+from sqlmodel import select
+
+
+class UserResult:
+    def __init__(self, username: str, phone: str) -> None:
+        self.username: str = username
+        self.phone: str = phone
 
 
 class UserRepository(UserRepositoryInterface):
@@ -22,6 +30,24 @@ class UserRepository(UserRepositoryInterface):
             try:
                 user = db_connection.session.query(User).filter_by(phone=phone).first()
                 return user
+            except Exception as exception:
+                db_connection.session.rollback()
+                raise exception
+
+    @classmethod
+    def get_users(cls, list_of_phones: list) -> List[UserResult]:
+        with DBConnectionHandler() as db_connection:
+            try:
+                query = select(User).where(User.phone.in_(list_of_phones))
+                users = db_connection.session.exec(query)
+
+                users_list = []
+
+                for row in users:
+                    user_result = UserResult(row.username, row.phone)
+                    users_list.append(user_result)
+
+                return users_list
             except Exception as exception:
                 db_connection.session.rollback()
                 raise exception
