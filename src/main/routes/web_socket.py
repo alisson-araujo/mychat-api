efa4e04cf433a370a8c152dc1da17b122fb979ca
Alisson Araujo <alisson.ads@hotmail.com>
2024-01-14 22:17:06 -0300

    feat: Add user authentication and enable private messaging in WebSocket endpoint

--- src/main/routes/web_socket.py
+++ src/main/routes/web_socket.py
@@ -1,27 +1,32 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from typing import List
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Header
+from src.drivers.auth.security import get_current_user
+from typing import List, Annotated, Optional, Dict
 
 
 ws_router = APIRouter()
 
-clients: List[WebSocket] = []
+clients: Dict[str, WebSocket] = {}
 
 
 @ws_router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket, token: Optional[str] = Header(None)):
     await websocket.accept()
 
-    clients.append(websocket)
+    user = await get_current_user(token)
+
+    clients[user] = websocket
 
     try:
         while True:
-            data = await websocket.receive_text()
-            print(data)
-            for client in clients:
-                if client != websocket:
-                    await client.send_text(data)
+            data = await websocket.receive_json()
+            recipient = data["recipient"]
+            data = data["data"]
+            print(f"{user} sent: {data} to {recipient}")
+
+            if recipient in clients:
+                await clients[recipient].send_text(data)
 
     except WebSocketDisconnect:
-        clients.remove(websocket)
+        del clients[user]
         if not clients:
             print("No more clients, closing...")
